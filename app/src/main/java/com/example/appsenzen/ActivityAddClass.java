cae3294416bdfb6e9e25e6c708ea5649e6ed4d3c
Jonathan Seyr <senoriship@gmail.com>
2020-04-20 17:04:53 +0200

    Added editText and Confirmation Button on ActivityAddClass

--- app/src/main/java/com/example/appsenzen/ActivityAddClass.java
+++ app/src/main/java/com/example/appsenzen/ActivityAddClass.java
@@ -1,13 +1,26 @@
 package com.example.appsenzen;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivityAddClass extends AppCompatActivity {
-
+public class ActivityAddClass extends AppCompatActivity implements View.OnClickListener{
+    String className;
+    Button button;
+    EditText inputClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
+
+        inputClass = findViewById(R.id.editText);
+        button = findViewById(R.id.buttonAddClass);
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        className = inputClass.getText().toString();
     }
 }
