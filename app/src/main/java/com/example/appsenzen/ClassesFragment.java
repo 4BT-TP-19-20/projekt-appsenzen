37593071169c220a8b6654b642cccf36da9327ce
Julian Lamprecht <51859473+Juli-EXP@users.noreply.github.com>
2020-04-20 19:35:34 +0200

    Adding buttons dynamically works inside ClassesFragment.java

--- app/src/main/java/com/example/appsenzen/ClassesFragment.java
+++ app/src/main/java/com/example/appsenzen/ClassesFragment.java
@@ -3,25 +3,30 @@ package com.example.appsenzen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
+
 
 public class ClassesFragment extends Fragment {
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_classes, container, false);
-        Button buttonAddClass = view.findViewById(R.id.buttonAddClass);
+         view = inflater.inflate(R.layout.fragment_classes, container, false);
 
+
+        Button buttonAddClass = view.findViewById(R.id.buttonAddClass);
         buttonAddClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +35,12 @@ public class ClassesFragment extends Fragment {
             }
         });
 
+        for(int i = 0; i <= 20; ++i){
+            addButtons(i);
+        }
+
+
+
         return view;
     }
 
@@ -37,4 +48,25 @@ public class ClassesFragment extends Fragment {
     public void onPause() {
         super.onPause();
     }
+
+    private void addButtons(final int i) {
+
+        Button button = new Button(getContext());
+        button.setText("Button: "+ i);
+        LinearLayout linearLayout = view.findViewById(R.id.linearlayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(button, params);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"You pressed button: " + i,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
