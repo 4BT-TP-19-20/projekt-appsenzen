e10eeecebc130d4f9a1f82aaf81a114c06512af6
Jonathan Seyr <senoriship@gmail.com>
2020-04-21 10:54:02 +0200

    Added floating Dialog for adding class

--- app/src/main/java/com/example/appsenzen/MainActivity.java
+++ app/src/main/java/com/example/appsenzen/MainActivity.java
@@ -6,21 +6,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddClassDialog.addClassDialogListener{
+    TextView addClassName;
+    @Override
+    public void showToast(String classname) {
+        Toast.makeText(MainActivity.this,"Added '" +classname+"' to classes", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -91,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
         buttonAddClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openAddClassActivity();
+                //openAddClassActivity();
+                openDialog();
             }
         });
     }
@@ -124,6 +130,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
+    private void openDialog(){
+        AddClassDialog addClassDialog = new AddClassDialog();
+        addClassDialog.show(getSupportFragmentManager(), "example dialog");
+    }
 
 }
