a62e93c994e3794493a6ebcf834a6cb951766628
Therealdanjo <Therealdanjo@users.noreply.github.com>
2020-04-24 10:15:11 +0200

    Added Date and Time in dashboard fragment
    Next step: updating the date and time every second (thread needed)

--- app/src/main/java/com/example/appsenzen/DashboardFragment.java
+++ app/src/main/java/com/example/appsenzen/DashboardFragment.java
@@ -1,17 +1,23 @@
 package com.example.appsenzen;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class DashboardFragment extends Fragment {
     View view;
+    Date currentTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,24 @@ public class DashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        TextView currentDateTime = new TextView(getActivity());
+
+        currentTime = Calendar.getInstance().getTime();
+
+        String timeString = currentTime.toString();
+        String[] splitString = timeString.split(" ");
+        timeString = splitString[0] + " " + splitString[1] + " " + splitString[2] + " " + splitString[5] + " " + splitString[3];
+
+        currentDateTime.setText(timeString);
+        currentDateTime.setGravity(Gravity.CENTER);
+
+        LinearLayout linearLayout = view.findViewById(R.id.linearlayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        currentDateTime.setLayoutParams(params);
+
+        linearLayout.addView(currentDateTime, params);
 
 /*
         for(int i = 0; i <= 20; ++i){
